872dca255f6791383396d329fa5d61fe17cb24e7
Nissim Chekroun <nissim.chekroun@gmail.com>
2023-07-26 12:52:04 +0100

    feat(frontend): add next.js frontend which depends on the ts / wasm implementations

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+    config.experiments.asyncWebAssembly = true;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ frontend/src/components/life.tsx
@@ -0,0 +1,31 @@
+import { Universe } from "wasm-game-of-life";
+import { useEffect, useState } from "react";
+
+export default function GameOfLife() {
+  const [universe, setUniverse] = useState(Universe.new_empty(0, 0));
+
+  useEffect(() => {
+    setUniverse(Universe.new_random(100, 50));
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setUniverse((u) => {
+          console.time("Universe.step");
+          const newUniverse = u.step();
+          console.timeEnd("Universe.step");
+          return newUniverse;
+        }),
+      50
+    );
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <pre>{universe.render()}</pre>
+    </div>
+  );
+}
