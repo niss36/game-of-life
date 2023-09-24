5354a6ca8f8cf665a8d4e46b7694a9d0b8ff5e65
Nissim Chekroun <nissim.chekroun@gmail.com>
2023-09-24 12:33:40 +0100

    fix(frontend): add `basePath` to match GH pages setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/game-of-life",
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     const wasmModuleFilename = isServer
