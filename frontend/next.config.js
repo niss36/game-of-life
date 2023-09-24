35d8bf6e3a14a8fb534b7eda0e62b1759713361c
Nissim Chekroun <nissim.chekroun@gmail.com>
2023-09-24 11:39:16 +0100

    fix(frontend): fix `next build` not working due to WASM

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  webpack: (config) => {
-    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+  webpack: (config, { isServer }) => {
+    const wasmModuleFilename = isServer
+      ? "./../static/wasm/[modulehash].wasm"
+      : "static/wasm/[modulehash].wasm";
+    config.output.webassemblyModuleFilename = wasmModuleFilename;
     config.experiments.asyncWebAssembly = true;
 
     return config;
